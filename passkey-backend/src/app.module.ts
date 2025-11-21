ebbe54310426f55c24bc47584242628da9f07219
Evgenia Blajer <evgenia.m.golubeva@gmail.com>
2025-11-21 15:43:10 +0100

    set up graphql and prisma

--- passkey-backend/src/app.module.ts
+++ passkey-backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // GraphQL
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false, // playground deprecated in Apollo
+      graphiql: true,
+      autoSchemaFile: `${process.cwd()}src/schema.gql`,
+      sortSchema: true,
+    }),
+    // .env configuration
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
