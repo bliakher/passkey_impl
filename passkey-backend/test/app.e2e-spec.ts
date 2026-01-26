60c2916f3ab6680580f94de9bd81878de20e7295
Evgenia Blajer <evgenia.m.golubeva@gmail.com>
2026-01-26 18:21:31 +0100

    add BE test

--- passkey-backend/test/app.e2e-spec.ts
+++ passkey-backend/test/app.e2e-spec.ts
@@ -1,13 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { GraphQLResponse } from './models/response-body.model';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('GraphQL E2E', () => {
+  let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -16,10 +16,36 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should perform signup and login', async () => {
+    const signupMutation = `
+      mutation {
+        signup(email: "test@example.com", password: "1234")
+      }
+    `;
+
+    await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: signupMutation })
+      .expect(200);
+
+    const loginMutation = `
+      mutation {
+        login(email: "test@example.com", password: "1234")
+      }
+    `;
+
+    const loginResponse = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: loginMutation })
+      .expect(200);
+
+    type LoginResponse = { login: string };
+    const body = loginResponse.body as GraphQLResponse<LoginResponse>;
+
+    expect(body.data?.login).toBeDefined();
   });
 });
